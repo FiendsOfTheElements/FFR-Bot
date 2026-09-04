989629766acc13b6410d99779e07fef1621ce60d
Mike B <bowman.michael@gmail.com>
2026-09-04 00:09:16 -0400

    Adding 8ball command

--- src/cogs/misc.py
+++ src/cogs/misc.py
@@ -1,5 +1,5 @@
 import re
-from math import ceil
+from math import ceil, floor
 from random import random
 from discord.ext import commands
 import constants
@@ -56,3 +56,41 @@ class MiscCommandCog(commands.Cog):
         else:
             coinres = "Tails"
         await ctx.message.channel.send(f"Coin landed on: **{format(coinres)}**")
+
+    @commands.command()
+    async def eightball(self, ctx, *, question):
+        eightball_responses = [
+            "It is certain.",
+            "It is decidedly so.",
+            "Without a doubt.",
+            "Yes - definitely.",
+            "You may rely on it.",
+            "As I see it, yes.",
+            "Most likely.",
+            "Outlook good.",
+            "Yes.",
+            "Signs point to yes.",
+            "Reply hazy, try again.",
+            "Ask again later.",
+            "Better not tell you now.",
+            "Cannot predict now.",
+            "Concentrate and ask again.",
+            "Don't count on it.",
+            "My reply is no.",
+            "My sources say no.",
+            "Outlook not so good.",
+            "Very doubtful.",
+            "Absolutely, positively, unequivocally, definitely maybe.",
+            "Don't ask questions you don't want to know the answer to.",
+            "Yes, but only if you're lucky.",
+            "Signs point to yes, but you need a nap first.",
+            "No... but don't take it personally.",
+            "Maybe, but only if you're wearing a hat.",
+            "I'm not sure, but I think you should ask again later.",
+            "Absolutely... as long as you don't cheat.",
+            "The universe says not today.",
+            "Lu... pa... ?",
+        ]
+        await ctx.message.channel.send(
+            f"Magic 8-ball says: **{eightball_responses[floor(random() * len(eightball_responses))]}**"
+        )
